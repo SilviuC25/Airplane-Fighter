14c72bd12d2b58c61543a800006a513054bf2b6f
VilutSilviu <vilut.chisalita@smartboxdigital.com>
2024-05-08 20:46:56 +0200

    Airplane Fighter Game

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import GameContainer from './GameContainer.tsx';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center text-primary my-5'>Airplane Fighter Game</h1>
+      <GameContainer /> 
+    </>
+  )
+}
+
+export default App
